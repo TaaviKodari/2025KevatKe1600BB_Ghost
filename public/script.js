1c3866c24f5be97d03e9e4a522a44021a3c49a65
TaaviKodari <taavi.makela@gmail.com>
2025-02-12 17:02:39 +0200

    game-board css

--- public/script.js
+++ public/script.js
@@ -3,4 +3,6 @@ document.getElementById('new-game-btn').addEventListener('click',startGame);
 
 function startGame(){
     console.log('Klikattu');
+    document.getElementById('intro-screen').style.display = 'none';
+    document.getElementById('game-screen').style.display = 'block'
 }
