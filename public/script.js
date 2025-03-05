41ae04787377d18e78050528c3a81f774e5a2cbe
TaaviKodari <taavi.makela@gmail.com>
2025-03-05 17:03:21 +0200

    calculatacellsize tehty

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 const BOARD_SIZE = 15;
+const cellSize = calculateCellSize();
 let board;
 
 document.getElementById('new-game-btn').addEventListener('click',startGame);
@@ -9,6 +10,8 @@ function startGame(){
     document.getElementById('game-screen').style.display = 'block'
 
     board = generateRandomBoard();
+
+    drawBoard(board);
 }
 
 function generateRandomBoard(){
@@ -17,11 +20,41 @@ function generateRandomBoard(){
     for(let y = 0; y < BOARD_SIZE; y++){
         for(let x = 0; x < BOARD_SIZE; x++){
             if(y === 0){
-                newBoard[y][x] = 'W';
+                newBoard[y][x] = 'W'; //W tarkoittaa seinää
             }
         }
     }
 
+
     console.log(newBoard);
     return newBoard;
+}
+
+function drawBoard(board){
+    const gameBoard = document.getElementById('game-board');
+    gameBoard.style.gridTemplateColumns = `repeat(${BOARD_SIZE},1fr)`;
+
+    for(let y = 0; y < BOARD_SIZE; y++){
+        for(let x = 0; x < BOARD_SIZE; x++){
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.style.width = cellSize + "px";
+            cell.style.height = cellSize + "px";
+            if(getCell(board,x,y) === 'W'){
+                cell.classList.add('wall');
+            }
+
+            gameBoard.appendChild(cell);
+        }
+    }
+}
+
+function getCell(board,x,y){
+    return board[y][x];
+}
+
+function calculateCellSize(){
+    const screenSize = Math.min(window.innerWidth,window.innerHeight);
+    const gameBoardSize = 0.95 * screenSize;
+    return gameBoardSize / BOARD_SIZE;
 }
